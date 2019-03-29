ef3d592d692fd1c4e6b28013935886418e722a8d
KALLY\vytautas.pranskunas <vytautas.pranskunas@kallysoft.ch>
2019-03-29 09:39:21 +0100

    Added tests for persist-storage plugin

--- spec/storage.spec.ts
+++ spec/storage.spec.ts
@@ -1,6 +1,11 @@
 import { StateHistory } from './../src/state/history';
 import { Store } from './../src/store/store';
 import { ReactStateTestBed } from '../src/react-state.test-bed';
+import { timer } from 'rxjs';
+import { tap, map } from 'rxjs/operators';
+import { PersistStateStorage } from '../src/store/plugins/persist-state.plugin';
+
+jest.useFakeTimers();
 
 describe('Storage', () => {
     let store: Store<any>;
@@ -10,7 +15,7 @@ describe('Storage', () => {
         store = ReactStateTestBed.createStore(initialState);
     });
 
-    it('should add state', () => {
+    it('should save state', () => {
         store.select(['layout']).storage.save({ key: 'testKey' });
         expect(<any>localStorage.getItem('state::testKey')).toBe('{"test":"test"}');
     });
@@ -39,6 +44,96 @@ describe('Storage', () => {
         store.select(['layout']).storage.remove({ key: 'remove-item' });
 
         expect(<any>localStorage.getItem('remove-item')).toBeNull();
+    });
+
+    describe('when delayed', () => {
+        const delay = 2000;
+        let storage = {
+            clear: () => timer(delay).pipe(tap(_ => localStorage.clear())),
+            getItem: (key: string) => timer(delay).pipe(map(_ => localStorage.getItem(key))),
+            removeItem: (key: string) => timer(delay).pipe(tap(_ => localStorage.removeItem(key))),
+            setItem: (key: string, value: any) => timer(delay).pipe(tap(_ => localStorage.setItem(key, value))),
+        } as PersistStateStorage;
+
+        it('should notify observer after state is saved', (done) => {
+            store.select(['layout'])
+                .storage.save({
+                    key: 'testKey',
+                    storageConfig: {
+                        storage: storage,
+                        getKeys: () => Object.keys((localStorage as unknown as LocalStorageMock).store)
+                    }
+                }).subscribe(data => {
+                    expect(data.key).toEqual('state::testKey');
+                    expect(data.data).toMatchObject({"test": "test"});
+                    done();
+                });
+
+            jest.runTimersToTime(delay);
+        });
+
+        it('should notify observer after state is loaded', (done) => {
+            const layoutStore = store.select(['layout']);
+
+            layoutStore.storage.save({key: 'testKey'});
+
+            layoutStore
+                .storage.load({
+                    key: 'testKey',
+                    storageConfig: {
+                        storage: storage,
+                        getKeys: () => Object.keys((localStorage as unknown as LocalStorageMock).store)
+                    }
+                }).subscribe(data => {
+                    expect(data.key).toEqual('state::testKey');
+                    expect(JSON.parse(data.data)).toMatchObject({"test": "test"});
+                    done();
+                });
+
+            jest.runTimersToTime(delay);
+        });
+
+        it('should notify observer after item is removed from storage', (done) => {
+            const layoutStore = store.select(['layout']);
+
+            layoutStore.storage.save({key: 'testKey'});
+
+            layoutStore
+                .storage.remove({
+                    key: 'testKey',
+                    storageConfig: {
+                        storage: storage,
+                        getKeys: () => Object.keys((localStorage as unknown as LocalStorageMock).store)
+                    }
+                }).subscribe(key => {
+                    expect(key).toEqual('state::testKey');
+                    done();
+                });
+
+            jest.runTimersToTime(delay);
+        });
+
+        it('should notify observer after storage is cleared', (done) => {
+            const layoutStore = store.select(['layout']);
+
+            layoutStore.storage.save({key: 'testKey'});
+            layoutStore.storage.save({key: 'testKey2'});
+
+            layoutStore
+                .storage.clear({
+                    storageConfig: {
+                        storage: storage,
+                        getKeys: () => Object.keys((localStorage as unknown as LocalStorageMock).store)
+                    }
+                }).subscribe(keys => {
+                    expect(keys.length).toBe(2);
+                    expect(keys[0]).toEqual('state::testKey');
+                    expect(keys[1]).toEqual('state::testKey2');
+                    done();
+                });
+
+            jest.runTimersToTime(delay);
+        });
     });
 });
 
